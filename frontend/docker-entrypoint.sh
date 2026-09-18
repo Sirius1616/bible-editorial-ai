7130456e743671b3f3731b385f25722677a947fd
Sirius1616 <siriusa1.615@gmail.com>
2026-09-18 01:24:39 +0100

    debug: log frontend runtime port and html contents on boot

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -5,4 +5,8 @@ PORT="${PORT:-80}"
 BACKEND_URL="${BACKEND_URL:-http://localhost:8000}"
 envsubst '${PORT} ${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
+echo "[entrypoint] PORT=${PORT} BACKEND_URL=${BACKEND_URL}" >&2
+echo "[entrypoint] files in html:" >&2
+ls -la /usr/share/nginx/html | head -20 >&2
+
 exec nginx -g "daemon off;"
